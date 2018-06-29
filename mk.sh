b312c63caa1c5bfa57e888f99749605b6841801c
Ke Sun <sk.alvin.x@gmail.com>
2018-06-29 14:11:45 +0800

    Modify mk.sh and use gcc-5.

--- mk.sh
+++ mk.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 export ARCH=arm64
-export CROSS_COMPILE=/opt/gcc-linaro-aarch64-none-elf-4.8-2013.11_linux/bin/aarch64-none-elf-
+export CROSS_COMPILE=/opt/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-elf/bin/aarch64-elf-
 #OUTPUT=build-phicomm-n1
 INSTALL_TMP=./_install
 
 if [ ! -x ${CROSS_COMPILE}gcc ]; then
-    wget -c http://openlinux.amlogic.com:8000/deploy/gcc-linaro-aarch64-none-elf-4.8-2013.11_linux.tar
-    sudo tar -xf gcc-linaro-aarch64-none-elf-4.8-2013.11_linux.tar -C /opt
+    wget -c https://releases.linaro.org/components/toolchain/binaries/5.5-2017.10/aarch64-elf/gcc-linaro-5.5.0-2017.10-x86_64_aarch64-elf.tar.xz
+    sudo tar -xf gcc-linaro-5.5.0-2017.10-x86_64_aarch64-elf.tar.xz -C /opt
 fi
 
 #if [ ! -d $OUTPUT ]; then mkdir -p $OUTPUT; fi
